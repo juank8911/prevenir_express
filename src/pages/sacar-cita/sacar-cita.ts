ae4c06e8d1c90fce8a6189804af107aa10d88c7d
juank8911 <juancarlos_891111@hotmail.com>
2018-09-24 11:40:09 -0500

    sacar sitas

--- src/pages/sacar-cita/sacar-cita.ts
+++ src/pages/sacar-cita/sacar-cita.ts
@@ -21,12 +21,12 @@ export class SacarCitaPage {
   maniana;
   tarde;
   hr:boolean = false;
-  
+
   constructor(public navCtrl: NavController, public navParams: NavParams,private api : ApiProvider) {
 
     this.today = moment(new Date().toISOString()).format('YYYY-M-DD');
     this.horarios();
-      
+
   }
 
   ionViewDidLoad() {
@@ -36,16 +36,16 @@ export class SacarCitaPage {
   hola(){
     console.log("hola");
   }
-  
+
   onDaySelect(ev)
   {
     console.log(ev);
-      
+
     let numero = parseInt(ev.month)+1;
     this.fecha = ev.year + "-" + numero + "-" +ev.date;
 
      let today = new Date(this.today).getTime();
-      let fecha = new Date(this.fecha).getTime();
+     let fecha = new Date(this.fecha).getTime();
 
       if(fecha >= today)
       {
@@ -54,7 +54,7 @@ export class SacarCitaPage {
         this.hr=true;
       }
       this.horarios();
-  
+
   }
 
   horarios(){
@@ -66,7 +66,7 @@ export class SacarCitaPage {
         let hors2 = data[1];
         this.tarde = hors2.tardes;
         console.log(hors2);
-      
+
       },(err)=>{console.log(err);});
     }else{
       this.api.getHorario(this.fecha).subscribe((data)=>{
@@ -74,10 +74,10 @@ export class SacarCitaPage {
         this.maniana = hors.maniana;
         let hors2 = data[1];
         this.tarde = hors2.tardes;
-        
+
       },(err)=>{console.log(err);});
     }
-  
+
   }
 
 
